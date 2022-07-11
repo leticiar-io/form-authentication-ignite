ed543c9bb3945f0a2e0ed1887f010c001b9d0111
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-07-11 10:06:11 -0300

    feat: adicionando o useContext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,76 +1,105 @@
-import { Stack, Flex, Heading, Button, Text, Image, Checkbox, FormControl, FormLabel, Input, Link } from '@chakra-ui/react'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import {
+  Stack,
+  Flex,
+  Heading,
+  Button,
+  Text,
+  Image,
+  Input} from "@chakra-ui/react";
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 
-const Home: NextPage = () => {
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmitForm(event : FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
+
   return (
-    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-      <Flex p={8} flex={1} align={'center'} justify={'center'}>
-        <Stack spacing={6} w={'full'} maxW={'lg'}>
-          <Heading fontSize={{ base: '2xl', md: '3xl', lg: '4xl' }}>
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Heading fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}>
             <Text
-              as={'span'}
-              position={'relative'}
+              as={"span"}
+              position={"relative"}
               _after={{
                 content: "''",
-                width: 'full',
-                position: 'absolute',
+                width: "full",
+                position: "absolute",
                 bottom: 1,
                 left: 0,
-                bg: 'purple.400',
-                zIndex: -1,
-              }}>
+                bg: "purple.400",
+                zIndex: -1
+              }}
+            >
               Bem-vindo(a), novamente!
             </Text>
-            <br />{' '}
-            <Text color={'purple.600'} as={'span'}>
+            <br />{" "}
+            <Text color={"purple.600"} as={"span"}>
               Faça aqui seu Login 😄
-            </Text>{' '}
+            </Text>{" "}
           </Heading>
-         
 
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>E-mail</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Senha</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Lembre de mim</Checkbox>
-                <Link color={'blue.400'}>Esqueceu a senha?</Link>
-              </Stack>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Fazer Login
-              </Button>
+          <Flex
+            as="form"
+            width="100%"
+            maxWidth={360}
+            borderRadius={8}
+            flexDir="column"
+            onSubmit={handleSubmitForm}
+          >
+            <Stack spacing="4">
+              <Input
+                name="email"
+                type="email"
+                label="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="chicomotoquinha@gmail.com"
+              />
+              <Input
+                name="password"
+                type="password"
+                label="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="********"
+              />
             </Stack>
-          </Stack>
 
+            <Button
+              type="submit"
+              mt="6"
+              colorScheme="purple"
+            >
+              Entrar
+            </Button>
+          </Flex>
         </Stack>
       </Flex>
       <Flex flex={1}>
         <Image
-          alt={'Login Image'}
-          objectFit={'cover'}
-          src={"https://www.expressivesuite.com/assets/posts/2021-09-14/landscape.svg"}
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={
+            "https://www.expressivesuite.com/assets/posts/2021-09-14/landscape.svg"
+          }
           maxHeight={"100vh"}
           width={"100%"}
         />
       </Flex>
     </Stack>
-  )
+  );
 }
-
-export default Home;
