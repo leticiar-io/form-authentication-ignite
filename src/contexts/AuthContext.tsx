422c9d49d28e92cfbda7e405a44106f19fb322a0
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-07-11 10:05:43 -0300

    feat: adicionando o authcontext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,66 @@
+import { createContext, ReactNode, useState } from "react";
+import Router from "next/router";
+import { setCookie } from "nookies";
+import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  isAuthenticated: boolean;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "ateex.token", token, {
+        maxAge: 60 * 60 * 24 * 30, //1 mês ou 30 dias
+        path: "/"
+      })
+
+      setCookie(undefined, "ateex.refreshToken", refreshToken)
+
+      setUser({
+        email,
+        permissions,
+        roles
+      });
+
+      Router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
